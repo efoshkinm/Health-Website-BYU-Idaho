79f9b7731cdad51f8122fcc8e5a8f3c5ec563c1d
efoshkinm <efo13001@byui.edu>
2016-11-27 15:51:36 -0700

    hjk

--- jsonweather-html/scripts/modules.js
+++ jsonweather-html/scripts/modules.js
@@ -2,6 +2,6 @@
 // Pathes may need altered depending on site structure
 $(function () {
   $("#page-header").load("../modules/header.html");
-  $("#page-nav").load("../modules/navigation.html");
+  $("#page-nav").load("../../modules/navigation.html");
   $("#footer-content").load("../modules/footer.html");
 });
