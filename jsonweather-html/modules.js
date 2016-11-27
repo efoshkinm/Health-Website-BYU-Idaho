b972453576176f52b3c26cf2084a80a017f14310
efoshkinm <efo13001@byui.edu>
2016-11-27 15:48:51 -0700

    ergregre

--- jsonweather-html/modules.js
+++
@@ -1,7 +0,0 @@
-// Include modules into the html file
-// Pathes may need altered depending on site structure
-$(function () {
-  $("#page-header").load("header.html");
-  $("#page-nav").load("navigation.html");
-  $("#footer-content").load("footer.html");
-});
--- jsonweather-html/scripts/remote_wu.js
+++ jsonweather-html/scripts/remote_wu.js
@@ -8,10 +8,10 @@ $(function () {
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(function (position) {
         var lat = position.coords.latitude;
-        var long = position.coords.longitude;
+        var lon = position.coords.longitude;
 
         // Call the getData function, send the lat and long
-        getData(lat, long);
+        getData(lat, lon);
 
       });
     } else {
@@ -21,47 +21,33 @@ $(function () {
   })();
 
   // Get the data from the wunderground API
-  function getData(lat, long){
+  function getData(lat, lon){
     $.ajax({
-	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.5.1/jquery.min.js"></script>
-	<script>
-	jQuery(document).ready(function($) {
-	  $.ajax({
-	  url : 'http://api.wunderground.com/api/Your_Key/geolookup/conditions/q/' + lat +',' + long + '.json',
-	  dataType : "jsonp",
-	  success : function(data) {
-		  console.log(data);
-	  var location = data['location']['city'] + ',' + data['location']['state'];
+	  url:'https://api.wunderground.com/api/aebb2a7c6f18a650/geolookup/conditions/q/' + lat + ',' + lon + '.json',
+	  dataType: "jsonp",
+	  success: function(data){
+	
+	  var locate = data['location']['city'] + ',' + data['location']['state'];
 	  var temp = data['current_observation']['temp_f'];
-	  var current = data ['current_observation']['weather'];
-	  var tz_long = data ['nearby_weather_stations'] ['tz_long'];
-	  var windchill_c = data ['current_observation'] ['windchill_c'];
-	  var windchill_f = data ['current_observation'] ['windchill_f'];
-	  var wind_mph = data ['current_observation'] ['wind_mph'];
+	  var current = data['current_observation']['weather'];
+	  var windchill_c = data['current_observation']['windchill_c'];
+	  var windchill_f = data['current_observation']['windchill_f'];
+	  var wind_mph = data['current_observation']['wind_mph'];
 	  
-	  $('#cityDisplay').html('location');
-	  $('#currentTemp').html('Math.round(temp)+"&#176;F"');
-	  $('#summary').html(toTitleCase(current));
-	  $('#add1').html('Time zone: ' + (tz_long));
-	  $('#add2').html('Windchill in Celsius: ' + (windchill_c) + 'C');
-	  $('#add3').html('Windchill in Farenheit: ' + (windchill_f) + '&#176;F');
-	  $('#add3').html('Wind miles per hour: ' + (wind_mph) + 'mph');
-	  
+	  $("#cityDisplay").html(data['location']['city']+", "+data['location']['state']);
+	  $("#currentTemp").html(Math.round(temp)+'&#176;F');
+	  $("#summary").html(toTitleCase(current));
+	  $("#add2").html('Windchill in Celsius: ' + (windchill_c) + 'C&#176;');
+	  $("#add3").html('Windchill in Farenheit: ' + (windchill_f) + '&#176;F');
+	  $("#add4").html('Wind miles per hour: ' + (wind_mph) + 'mph');
+	
   }
   });
-});
-</script>
 
-
-
-
-
-
-      $("#cover").fadeOut(250);
+  $("#cover").fadeOut(250);
+      
     }
-           });
-
-  }
+ 
 
   // A function for changing a string to TitleCase
   function toTitleCase(str){
