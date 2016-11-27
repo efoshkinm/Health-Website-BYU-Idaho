f6cae59af29ce4097d6fa2f6d26f426d656540f7
efoshkinm <efo13001@byui.edu>
2016-11-27 00:00:50 -0700

    hjkhjh

--- jsonweather-html/remote_wu.js
+++ jsonweather-html/remote_wu.js
@@ -27,7 +27,7 @@ $(function () {
 	  dataType: "jsonp",
 	  success: function(data){
 	console.log(data);
-	  var location = data['location']  + ',' + data['location']['state'];
+	  var location = data['location'] ['city'] + ',' + data['location']['state'];
 	  var temp = data['current_observation']['temp_f'];
 	  var current = data ['current_observation']['weather'];
 	  var tz_long = data ['nearby_weather_stations'] ['tz_long'];
@@ -35,13 +35,13 @@ $(function () {
 	  var windchill_f = data ['current_observation'] ['windchill_f'];
 	  var wind_mph = data ['current_observation'] ['wind_mph'];
 	  
-	  $('#cityDisplay').html('location');
-	  $('#currentTemp').html('Math.round(temp)+"&#176;F"');
-	  $('#summary').html(toTitleCase(current));
-	  $('#add1').html('Time zone: ' + (tz_long));
-	  $('#add2').html('Windchill in Celsius: ' + (windchill_c) + 'C');
-	  $('#add3').html('Windchill in Farenheit: ' + (windchill_f) + '&#176;F');
-	  $('#add4').html('Wind miles per hour: ' + (wind_mph) + 'mph');
+	  $("#cityDisplay").html(location);
+	  $("#currentTemp").html('Math.round(temp)+"&#176;F"');
+	  $("#summary").html(toTitleCase(current));
+	  $("#add1").html('Time zone: ' + (tz_long));
+	  $("#add2").html('Windchill in Celsius: ' + (windchill_c) + 'C');
+	  $("#add3").html('Windchill in Farenheit: ' + (windchill_f) + '&#176;F');
+	  $("#add4").html('Wind miles per hour: ' + (wind_mph) + 'mph');
 	  
   }
   });
