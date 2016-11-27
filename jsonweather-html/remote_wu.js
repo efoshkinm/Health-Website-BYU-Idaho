ac8759be96feca7c995b9ee63f912d2ea3d0efa9
efoshkinm <efo13001@byui.edu>
2016-11-27 15:08:09 -0700

    eg

--- jsonweather-html/remote_wu.js
+++ jsonweather-html/remote_wu.js
@@ -39,9 +39,9 @@ $(function () {
 	  $("#currentTemp").html(Math.round(temp)+'&#176;F');
 	  $("#summary").html(toTitleCase(current));
 	  $("#add1").html('Time zone: ' + (tz_long));
-	  $("#add2").html('Windchill in Celsius: ' + windchill_c + 'C');
-	  $("#add3").html('Windchill in Farenheit: ' + windchill_f + '&#176;F');
-	  $("#add4").html('Wind miles per hour: ' + wind_mph + 'mph');
+	  $("#add2").html('Windchill in Celsius: ' + (windchill_c) + 'C');
+	  $("#add3").html('Windchill in Farenheit: ' + (windchill_f) + '&#176;F');
+	  $("#add4").html('Wind miles per hour: ' + (wind_mph) + 'mph');
 	  
   }
   });
