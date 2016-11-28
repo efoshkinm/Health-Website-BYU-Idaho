beadfab51f1261db425ddd4572ca5571efb76e17
efoshkinm <efo13001@byui.edu>
2016-11-28 10:22:12 -0700

    dfdfd

--- jsonweather-html/scripts/modules.js
+++ jsonweather-html/scripts/modules.js
@@ -1,7 +1,7 @@
 // Include modules into the html file
 // Pathes may need altered depending on site structure
 $(function () {
-  $("#page-header").load("/jsonweather-html/modules/modules/header.html");
+  $("#page-header").load("/jsonweather-html/modules/header.html");
   $("#page-nav").load("/jsonweather-html/modules/navigation.html");
-  $("#footer-content").load("/jsonweather-html/modules/modules/footer.html");
+  $("#footer-content").load("/jsonweather-html/modules/footer.html");
 });
