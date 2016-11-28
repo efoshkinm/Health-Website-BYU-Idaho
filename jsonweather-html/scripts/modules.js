d1548c8a756b86180a8928ac9a1bcd65231ee7be
efoshkinm <efo13001@byui.edu>
2016-11-28 10:18:52 -0700

    edrfgetret

--- jsonweather-html/scripts/modules.js
+++ jsonweather-html/scripts/modules.js
@@ -1,7 +1,7 @@
 // Include modules into the html file
 // Pathes may need altered depending on site structure
 $(function () {
-  $("#page-header").load("../modules/header.html");
+  $("#page-header").load("/jsonweather-html/modules/modules/header.html");
   $("#page-nav").load("/jsonweather-html/modules/navigation.html");
-  $("#footer-content").load("../modules/footer.html");
+  $("#footer-content").load("/jsonweather-html/modules/modules/footer.html");
 });
