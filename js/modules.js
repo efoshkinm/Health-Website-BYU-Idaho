aeb3aa4fab20f7d1d4e15728d01675f00d5d4ec4
efoshkinm <efo13001@byui.edu>
2016-12-01 19:13:52 -0700

    dfgdfgdf

--- js/modules.js
+++ js/modules.js
@@ -1,7 +1,7 @@
 $(function() {
-$("#current-header").load("../modules/current-header.html");
-$("#franklin-header").load("../modules/franklin-header.html");
-$("#greenville-header").load("../modules/greenville-header.html");
-$("#page-nav").load("../modules/nav.html");
-$("#page-footer").load("../modules/footer.html");
+$("#current-header").load("/modules/current-header.html");
+$("#franklin-header").load("/modules/franklin-header.html");
+$("#greenville-header").load("/modules/greenville-header.html");
+$("#page-nav").load("/modules/nav.html");
+$("#page-footer").load("/modules/footer.html");
 });
