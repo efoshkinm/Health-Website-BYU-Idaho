691b8f818b10f3c43fa977371bae6ecb17a06d37
efoshkinm <efo13001@byui.edu>
2016-12-01 21:19:28 -0700

    fghfhj

--- js/search.js
+++ js/search.js
@@ -91,5 +91,6 @@ $("#page-nav").on("click", "a", function (evt) {
  // A function for changing a string to TitleCase
   function toTitleCase(str){
     return str.replace(/\w+/g, function(txt){return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();});
-  }
+  };
+  
 
