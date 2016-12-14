856ffed874ec7db67f1339cffb01b57c700ae4bf
efoshkinm <efo13001@byui.edu>
2016-12-14 10:53:32 -0700

    uiuuu

--- project/script/workout_json.js
+++ project/script/workout_json.js
@@ -2,16 +2,16 @@ $(function () {
 	console.log("working")
 	$.getJSON("/project/script/health.json", function (data){
 		console.log(data);
-	  var push_ups = data['Exercises']['Push-ups'];
-	  var pull_ups = data['Exercises']['Pull-ups'];
-	  var chin_ups = data['Exercises']['Chin-ups'];
+	  var pushups = data['Exercises']['Push-ups'];
+	  var pullups = data['Exercises']['Pull-ups'];
+	  var chinups = data['Exercises']['Chin-ups'];
 	  var run = data['Exercises']['Run'];
 	  var jump = data['Exercises']['Jump'];
 	  var abs = data['Exercises']['Abs'];
 	  
-	  $("#push_ups").html(push_ups);
-	  $("#pull_ups").html(pull_ups);
-	  $("#chin_ups").html(chin_ups);
+	  $("#pushups").html(pushups);
+	  $("#pullups").html(pullups);
+	  $("#chinups").html(chinups);
 	  $("#run").html(run);
 	  $("#jump").html(jump);
 	  $("#abs").html(abs);
