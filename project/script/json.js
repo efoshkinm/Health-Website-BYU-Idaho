2f87b47d753361e95dba134c7037be81b4f50313
efoshkinm <efo13001@byui.edu>
2016-12-14 10:17:15 -0700

    rtrrttr

--- project/script/json.js
+++ project/script/json.js
@@ -1,18 +1,17 @@
-$(function ()) {
+$(function () {
 	$.getJSON("/project/script/health.json", function (data){
 		console.log(data);
 	  var cabbage = data['Vegies']['0'];
-	  var garlic = data['Food']['Vegies']['1'];
-	  var potatoes = data['Food']['Vegies']['2'];
-	  var onions = data['Food']['Vegies']['3'];
-	  var tomatoes = data['Food']['Vegies']['4'];
+	  var garlic = data['Vegies']['1'];
+	  var potatoes = data['Vegies']['2'];
+	  var onions = data['Vegies']['3'];
+	  var tomatoes = data['Vegies']['4'];
 	  
-	  $("#cabbage").html(cabbage);
-	  $("#garlic").html(garlic);
-	  $("#potatoes").html(potatoes);
-	  $("#onions").html(onions);
-	  $("#tomatoes").html(tomatoes);
+	  $("#cabbage").html('cabbage');
+	  $("#garlic").html('garlic');
+	  $("#potatoes").html('potatoes');
+	  $("#onions").html('onions');
+	  $("#tomatoes").html('tomatoes');
 	
-  }
   });
- };
+  });
