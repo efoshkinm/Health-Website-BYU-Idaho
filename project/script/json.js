477d0893fc7293841838c057b460aaaea9df632b
efoshkinm <efo13001@byui.edu>
2016-12-14 09:32:02 -0700

    refefe

--- project/script/json.js
+++ project/script/json.js
@@ -7,9 +7,8 @@
 	  var cabbage = data['Food']['Vegies']['0'];
 	  var garlic = data['Food']['Vegies']['1'];
 	  var potatoes = data['Food']['Vegies']['2'];
-	  var potatoes = data['Food']['Vegies']['3'];
-	  var onions = data['Food']['Vegies']['4'];
-	  var tomatoes = data['Food']['Vegies']['5'];
+	  var onions = data['Food']['Vegies']['3'];
+	  var tomatoes = data['Food']['Vegies']['4'];
 	  
 	  $("#cabbage").html(cabbage);
 	  $("#garlic").html(garlic);
@@ -20,3 +19,4 @@
   }
   });
  }
+ getData();
